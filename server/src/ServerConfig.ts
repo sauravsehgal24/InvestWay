3c0de90f141f7ce1238189813023ffcfa1298233
sauravsehgal24 <sauravsehgal44@gmail.com>
2021-05-07 17:40:10 -0400

    sddsa

--- server/src/ServerConfig.ts
+++ server/src/ServerConfig.ts
@@ -12,72 +12,82 @@ import SERVER_CONFIG from "./config/server.config";
 import Response from "./config/HttpResponse";
 import get from "axios";
 
-export type IServerConfig = InstanceType<typeof ServerConfig>
-export class ServerConfig{
-    _context:string = "ServerConfig"
+export type IServerConfig = InstanceType<typeof ServerConfig>;
+export class ServerConfig {
+    _context: string = "ServerConfig";
     private app;
     private connection: Connection;
     private server: https.Server | http.Server;
 
-    constructor(connection: Connection){
+    constructor(connection: Connection) {
         this.app = express();
         this.initiateConfig([this.injectMiddleware]);
         this.connection = connection;
     }
-    private initiateConfig = (cbs: Function[]) =>  { 
+    private initiateConfig = (cbs: Function[]) => {
         cbs.map((cb) => cb());
     };
-    public initServer = () =>{
-        this.enableRoutes()
-        if(SERVER_CONFIG["IW_NODE_ENV"] === "local" || SERVER_CONFIG["IW_NODE_ENV"] === "development"){
+    public initServer = () => {
+        this.enableRoutes();
+        if (
+            SERVER_CONFIG["IW_NODE_ENV"] === "local" ||
+            SERVER_CONFIG["IW_NODE_ENV"] === "development"
+        ) {
             this.server = http.createServer(this.app);
             this.server.listen(SERVER_CONFIG["SERVER_IW_SERVER_PORT"], () => {
                 console.log(
-                    "\n---------------------------------------------------------------\nIW "
-                    +SERVER_CONFIG['IW_NODE_ENV']+" ts server listening at http://localhost:"+SERVER_CONFIG['SERVER_IW_SERVER_PORT']+
-                    "\n---------------------------------------------------------------\n"
+                    "\n---------------------------------------------------------------\nIW " +
+                        SERVER_CONFIG["IW_NODE_ENV"] +
+                        " ts server listening at http://localhost:" +
+                        SERVER_CONFIG["SERVER_IW_SERVER_PORT"] +
+                        "\n---------------------------------------------------------------\n"
                 );
             });
-    }
-    }
-    
-    private enableRoutes = () =>{
+        }
+    };
+
+    private enableRoutes = () => {
         const allRoutes = new RootRouter(this.connection).getRoutes();
-        this.app.use("/api",allRoutes);
-        if(SERVER_CONFIG["IW_NODE_ENV"] === "local" || SERVER_CONFIG["IW_NODE_ENV"] === "development"){
+        this.app.use("/api", allRoutes);
+        if (
+            SERVER_CONFIG["IW_NODE_ENV"] === "local" ||
+            SERVER_CONFIG["IW_NODE_ENV"] === "development"
+        ) {
             this.app.get("/", (req, res) => {
-                res.send("IW server active!"); 
+                res.send("IW server active!");
             });
         }
-        this.app.get("/request_c_code",(req,res)=>{
+        this.app.get("/request_c_code", (req, res) => {
             const payload = {
-                message:Response.OK.message,
-                refreshToken:SERVER_CONFIG["APP_IW_QS_REFRESH_TOKEN"],
-                qsAuthUrl:SERVER_CONFIG["APP_IW_QS_API_AUTH"],
-                callbackUrl:SERVER_CONFIG["APP_QS_AUTH_CALLBACK_URL"],
-                consumerKey:SERVER_CONFIG["APP_IW_QS_CONSMER_KEY"]
-            }
-            console.log(payload)
-            res.status(Response.OK.status).json(payload)
-        })
-        this.app.get("/test",(req,res)=>{
-            get("https://api07.iq.questrade.com/v1/accounts/52310463/positions", {
-            headers: {
-                // 'Content-Type': 'application/json',
-                // "Access-Control-Allow-Origin":"ORIGIN",
-                'Authorization':'Bearer DhgBfdS5COzm-5iJWl4N_PsoE_FHVUUF0'
-            }
-            }).then(response=>{
-                console.log(response.data)
-            })
-        })
-        
-    }
-    private injectMiddleware = () =>{
+                message: Response.OK.message,
+                refreshToken: SERVER_CONFIG["APP_IW_QS_REFRESH_TOKEN"],
+                qsAuthUrl: SERVER_CONFIG["APP_IW_QS_API_AUTH"],
+                callbackUrl: SERVER_CONFIG["APP_QS_AUTH_CALLBACK_URL"],
+                consumerKey: SERVER_CONFIG["APP_IW_QS_CONSMER_KEY"],
+            };
+            console.log(payload);
+            res.status(Response.OK.status).json(payload);
+        });
+        this.app.get("/test", (req, res) => {
+            get(
+                "https://api07.iq.questrade.com/v1/accounts/52310463/positions",
+                {
+                    headers: {
+                        // 'Content-Type': 'application/json',
+                        // "Access-Control-Allow-Origin":"ORIGIN",
+                        Authorization:
+                            "Bearer DhgBfdS5COzm-5iJWl4N_PsoE_FHVUUF0",
+                    },
+                }
+            ).then((response) => {
+                console.log(response.data);
+            });
+        });
+    };
+    private injectMiddleware = () => {
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(bodyParser.json());
         this.app.use(cors());
         this.app.use(compression());
-    }
-    
+    };
 }
--- server/src/api/UserRouter.ts
+++ server/src/api/UserRouter.ts
@@ -3,60 +3,55 @@ import { IUserService, UserService } from "../services/UserService";
 import { BaseRouter } from "./BaseRouter";
 import HttpResponse from "../config/HttpResponse";
 import { Util } from "../utils/Util";
-import{AuthService} from "../services/AuthService";
+import { AuthService } from "../services/AuthService";
 
 export class UserRouter extends BaseRouter<IUserService> {
-    private userService:IUserService
-    constructor(connection: Connection){
+    private userService: IUserService;
+    constructor(connection: Connection) {
         super(connection, "UserRouter");
         this.userService = new UserService(this.connection);
-        this.initiateAllRoutes([
-            this.getAllUsers,
-            this.authUser
-          ]);
+        this.initiateAllRoutes([this.getAllUsers, this.authUser]);
     }
- 
+
     // GET - All Users
-  private getAllUsers = () => {
-    this.router.get("/", async(req, res) => {
-      res.send(await this.userService.testUserService());
-    });
-  };
+    private getAllUsers = () => {
+        this.router.get("/", async (req, res) => {
+            res.send(await this.userService.testUserService());
+        });
+    };
 
-  private authUser = () =>{
-    this.router.post("/auth",async(req,res)=>{
-      const { email, password } = req.body;
-      const user = await this.userService.findUserByEmail(email)
-      if (!user) {
-        return res
-          .status(HttpResponse.Forbidden.status)
-          .json({ message: HttpResponse.Forbidden.message });
-      }
-      const isPassowrdCorrect: boolean = await Util.comparePassword(
-        password.toString(),
-        user.password.toString()
-      );
-      if(!isPassowrdCorrect){
-        return res
-          .status(HttpResponse.Forbidden.status)
-          .json({ message: HttpResponse.Forbidden.message });
-      }
-      const payload = {
-        userId:user._id,
-        _id:user._id,
-        email:user.email,
-        role:user.role
-      }
-      const token = AuthService.signToken(payload)
-      return res
-        .status(HttpResponse.OK.status)
-        .json({ token, isActivated: user.isActivated });
-    })
-  }
+    private authUser = () => {
+        this.router.post("/auth", async (req, res) => {
+            const { email, password } = req.body;
+            const user = await this.userService.findUserByEmail(email);
+            if (!user) {
+                return res
+                    .status(HttpResponse.Forbidden.status)
+                    .json({ message: HttpResponse.Forbidden.message });
+            }
+            const isPassowrdCorrect: boolean = await Util.comparePassword(
+                password.toString(),
+                user.password.toString()
+            );
+            if (!isPassowrdCorrect) {
+                return res
+                    .status(HttpResponse.Forbidden.status)
+                    .json({ message: HttpResponse.Forbidden.message });
+            }
+            const payload = {
+                userId: user._id,
+                _id: user._id,
+                email: user.email,
+                role: user.role,
+            };
+            const token = AuthService.signToken(payload);
+            return res
+                .status(HttpResponse.OK.status)
+                .json({ token, isActivated: user.isActivated });
+        });
+    };
 
-  public getuserRoutes = () => {
-    return this.router;
-  };
+    public getuserRoutes = () => {
+        return this.router;
+    };
 }
-
-    
