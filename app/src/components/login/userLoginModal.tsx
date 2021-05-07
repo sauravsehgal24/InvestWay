119426b07b2181ffa2eafa5b2860ce12a9328f6c
sauravsehgal24 <sauravsehgal44@gmail.com>
2021-05-07 17:37:33 -0400

    prettier config added

--- app/src/components/login/userLoginModal.tsx
+++ app/src/components/login/userLoginModal.tsx
@@ -1,15 +1,22 @@
 import * as React from "react";
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import { Button, Checkbox, Fade, FormControlLabel, makeStyles, Snackbar, TextField, Typography } from "@material-ui/core";
-
-
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import {
+    Button,
+    Checkbox,
+    Fade,
+    FormControlLabel,
+    makeStyles,
+    Snackbar,
+    TextField,
+    Typography,
+} from "@material-ui/core";
 
 type UserLoginProps = {
-    open:boolean,
-    handleUserLoginModal:()=>void,
-    type:string
-}
+    open: boolean;
+    handleUserLoginModal: () => void;
+    type: string;
+};
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -17,8 +24,8 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-      },
-      paper: {
+    },
+    paper: {
         backgroundColor: "white ",
         height: "500px",
         width: "400px",
@@ -26,142 +33,147 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-      },
-      loginButton: {
+    },
+    loginButton: {
         backgroundColor: "#808BEC",
         color: "#FFFFFF",
         fontSize: "15px",
         width: "80%",
         height: "12%",
         marginTop: "2%",
-      },
-      textField: {
+    },
+    textField: {
         width: "80%",
         marginTop: "3%",
-      },
-      googleButton: {
+    },
+    googleButton: {
         color: "#FFFFFF",
         fontSize: "15px",
         width: "80%",
         height: "12%",
         marginTop: "2%",
         marginBottom: "3%",
-      },
-  })); 
+    },
+}));
 
-const UserLogin:React.FC<UserLoginProps> = (props:UserLoginProps)=>{
-
-    const [snacBarObj, setSnackBarObj] = React.useState({open:false,message:""})
+const UserLogin: React.FC<UserLoginProps> = (props: UserLoginProps) => {
+    const [snacBarObj, setSnackBarObj] = React.useState({
+        open: false,
+        message: "",
+    });
     const classes = useStyles();
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [checked, setChecked] = React.useState(false);
     const handleChecked = async () => {
         if (
-          !checked &&
-          email.toString().trim() !== "" &&
-          password.toString().trim() !== ""
+            !checked &&
+            email.toString().trim() !== "" &&
+            password.toString().trim() !== ""
         ) {
-          localStorage.setItem("email", email);
-          localStorage.setItem("password", password);
-          setChecked(true);
+            localStorage.setItem("email", email);
+            localStorage.setItem("password", password);
+            setChecked(true);
         } else if (
-          checked ||
-          email.toString().trim() === "" ||
-          password.toString().trim() === ""
+            checked ||
+            email.toString().trim() === "" ||
+            password.toString().trim() === ""
         ) {
-          localStorage.removeItem("email");
-          localStorage.removeItem("password");
-          setChecked(false);
+            localStorage.removeItem("email");
+            localStorage.removeItem("password");
+            setChecked(false);
         }
-      };
-      const handelInput = (event, type) => {
+    };
+    const handelInput = (event, type) => {
         if (type === "email") {
-          setEmail(event.target.value);
+            setEmail(event.target.value);
         } else {
-          setPassword(event.target.value);
+            setPassword(event.target.value);
         }
-      };
-      const auth = () =>{
-          console.log(`EMAIL: ${email},  PASSWORD: ${password}, CHECKED: ${checked}`)
-      }
+    };
+    const auth = () => {
+        console.log(
+            `EMAIL: ${email},  PASSWORD: ${password}, CHECKED: ${checked}`
+        );
+    };
 
-      const fadeModal = ()=>{
-        return (  
+    const fadeModal = () => {
+        return (
             <React.Fragment>
-        <Typography variant="h3">Login</Typography>
-        <TextField
-          className={classes.textField}
-          value={email}
-          onChange={(event) => handelInput(event, "email")}
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-        />
-            <TextField
-              className={classes.textField}
-              value={password}
-              onChange={(event) => handelInput(event, "password")}
-              id="outlined-basic"
-              label="Password"
-              type="password"
-              variant="outlined"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  name="checked"
-                  color="primary"
-                  checked={checked}
-                  onChange={handleChecked}
+                <Typography variant="h3">Login</Typography>
+                <TextField
+                    className={classes.textField}
+                    value={email}
+                    onChange={(event) => handelInput(event, "email")}
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
                 />
-              }
-              label="Save Credentials"
-            />
-            <Button
-              onClick={() => auth()}
-              variant="contained"
-              color="primary" 
-              className={classes.loginButton}
-            >
-              Login
-            </Button>
+                <TextField
+                    className={classes.textField}
+                    value={password}
+                    onChange={(event) => handelInput(event, "password")}
+                    id="outlined-basic"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            name="checked"
+                            color="primary"
+                            checked={checked}
+                            onChange={handleChecked}
+                        />
+                    }
+                    label="Save Credentials"
+                />
+                <Button
+                    onClick={() => auth()}
+                    variant="contained"
+                    color="primary"
+                    className={classes.loginButton}
+                >
+                    Login
+                </Button>
             </React.Fragment>
-        )
-      }
+        );
+    };
 
     return (
         <React.Fragment>
-        <Snackbar
-            open={snacBarObj.open}
-            onClose={() => {
-                setSnackBarObj({ open: false, message: "" });
-            }}
-            message={snacBarObj.message}
-        />
-        <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={props.open}
-            onClose={props.handleUserLoginModal}
-            BackdropComponent={Backdrop}
-            closeAfterTransition={true}
-            BackdropProps={{
-              timeout: 500,
-            }}
-            className={classes.modal}
-        >
-            <Fade in={props.open}>
-          <div className={classes.paper}>
-             {
-                 props.type==="QS"?<h1>Comming Soon</h1>:fadeModal()
-             }
-          </div>
-        </Fade>
-        </Modal>
+            <Snackbar
+                open={snacBarObj.open}
+                onClose={() => {
+                    setSnackBarObj({ open: false, message: "" });
+                }}
+                message={snacBarObj.message}
+            />
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={props.open}
+                onClose={props.handleUserLoginModal}
+                BackdropComponent={Backdrop}
+                closeAfterTransition={true}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+                className={classes.modal}
+            >
+                <Fade in={props.open}>
+                    <div className={classes.paper}>
+                        {props.type === "QS" ? (
+                            <h1>Comming Soon</h1>
+                        ) : (
+                            fadeModal()
+                        )}
+                    </div>
+                </Fade>
+            </Modal>
         </React.Fragment>
-    )
-}
+    );
+};
 
-export default UserLogin
-
+export default UserLogin;
