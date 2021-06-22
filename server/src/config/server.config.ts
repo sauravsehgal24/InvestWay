a76e5090d21050d208059e68f45a5c737c388c15
sauravsehgal24 <sauravsehgal44@gmail.com>
2021-06-22 19:40:35 -0400

    changed env path

--- server/src/config/server.config.ts
+++ server/src/config/server.config.ts
@@ -7,7 +7,7 @@ const configMapperFunction = () => {
     if (!NODE_ENV || NODE_ENV === "local" || NODE_ENV === "development")
         dotenv.config({ path: path.join(__dirname, "../../../.env") });
     else if (NODE_ENV === "production")
-        dotenv.config({ path: path.join(__dirname, "../../../.env.prod") });
+        dotenv.config({ path: path.join(__dirname, "../../../../.env.prod") });
     let CONFIG = {};
     Object.keys(process.env).map((key) => {
         CONFIG[key] = process.env[key];
